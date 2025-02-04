1988f067564f69a54c459b83b6bd0ccb0fd31eec
danielrodrig86 <daniel.rodrigues1998@live.com>
2025-02-04 20:48:15 -0300

    Configure cypress cloud 1

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
+  projectId: '8yv889',
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
     env: {
